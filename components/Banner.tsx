f9b0e664ab78ac988e2270d52ced0dfbb5a6a974
@chonprakun <Lumworker@hotmail.com>
2025-01-18 19:18:00 +0700

    Add Experience component and remove ExperienceQChang; update package.json and next-env.d.ts

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,29 +1,88 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import { motion } from "framer-motion"; // นำเข้า motion
 
 const Banner: React.FC<{}> = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsFlipped((prev) => !prev); // Toggle flip state
+    }, 5000); // Flip every 10 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
   return (
-    <div className="flex flex-row items-center justify-center px-20 mt-[100px] z-[20] ">
-      <div className="flex flex-col  justify-center  text-center">
-        <div className="justify-center flex ">
-          <Image
-            priority
-            src="/Me.png"
-            height={250}
-            width={250}
-            alt="Aong Chonprakun"
-          />
+    <div className="flex flex-row items-center justify-center px-20 mt-[100px] z-[20]">
+      <div className="flex flex-col justify-center text-center">
+        <div className="justify-center flex">
+          <div
+            className="relative w-[250px] h-[250px] flex items-center justify-center"
+            style={{ perspective: "1000px" }} // Enables 3D effect
+          >
+            <motion.div
+              className="absolute w-full h-full flex items-center justify-center"
+              style={{
+                backfaceVisibility: "hidden",
+                transformStyle: "preserve-3d",
+              }}
+              animate={{
+                rotateY: isFlipped ? 180 : 0, // Flip effect
+              }}
+              initial={false} // ป้องกันการแสดงผลจากค่าเริ่มต้น
+              transition={{
+                duration: 1, // Time for the flip animation
+              }}
+            >
+              {/* Front Side */}
+              <Image
+                priority
+                src="/Me.png"
+                height={250}
+                width={250}
+                alt="Front Side"
+                onClick={() => setIsFlipped(!isFlipped)} // เพิ่ม onClick
+              />
+            </motion.div>
+
+            <motion.div
+              className="absolute w-full h-full flex items-center justify-center"
+              style={{
+                backfaceVisibility: "hidden",
+                transformStyle: "preserve-3d",
+                transform: "rotateY(180deg)",
+              }}
+              animate={{
+                rotateY: isFlipped ? 0 : 180, // Flip back effect
+              }}
+              initial={false} // ป้องกันการแสดงผลจากค่าเริ่มต้น
+              transition={{
+                duration: 1, // Time for the flip animation
+              }}
+            >
+              {/* Back Side */}
+              <Image
+                priority
+                src="/me3.webp"
+                height={250}
+                width={250}
+                alt="Back Side"
+                onClick={() => setIsFlipped(!isFlipped)} // เพิ่ม onClick
+              />
+            </motion.div>
+          </div>
         </div>
 
         <div className="flex flex-col gap-6 mt-10 cursor-pointer animate-bounce tracking-tighter text-7xl font-semibold text-white max-w-[600px] w-auto h-auto">
           Aong Chonprakun!
         </div>
         <div className="flex justify-center">
-          <p className="text-2xl font-medium tracking-tighter  text-gray-300 max-w-[600px]">
+          <p className="text-2xl font-medium tracking-tighter text-gray-300 max-w-[600px]">
             I do Code &{" "}
-            <span className="text-transparent font-semibold  bg-clip-text bg-gradient-to-r from-purple-500 to-orange-400">
+            <span className="text-transparent font-semibold bg-clip-text bg-gradient-to-r from-purple-500 to-orange-400">
               Chill
             </span>
           </p>
@@ -36,12 +95,14 @@ const Banner: React.FC<{}> = () => {
         </div>
 
         <p className="text-md text-gray-200 my-5 max-w-[600px]">
-        Software Engineer focused on web applications with expertise in React, Vue, Next.js, and Nuxt.js, dedicated to creating efficient and user-friendly experiences.
+          Software Engineer focused on web applications with expertise in React,
+          Vue, Next.js, and Nuxt.js, dedicated to creating efficient and
+          user-friendly experiences.
         </p>
         <div className="text-md flex justify-center">
           <button
             onClick={() => window.open("mailto:Lumworker@hotmail.com")}
-            className=" z-[1]     padding-20  hover:bg-white rounded-3xl  text-white font-semibold hover:text-black py-3 px-10  border-[0.1px] border-white hover:border-transparent "
+            className="z-[1] hover:bg-white rounded-3xl text-white font-semibold hover:text-black py-3 px-10 border-[0.1px] border-white hover:border-transparent"
           >
             Contact Me
           </button>
---
+++ components/Experience.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import React from "react";
+
+const Experience: React.FC<{}> = () => {
+  return (
+    <section id="experience">
+      <h2 className="text-white font-semibold text-center text-6xl   pt-[35px]">
+        EXPERIENCE
+      </h2>
+      <p className=" tracking-[0.5em] text-center text-transparent font-light pb-5  bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500  text-1xl ">
+        EXPLORE NOW
+      </p>
+      <div className=" container mx-auto 2xl ">
+        <div className="md:flex md:flex-row md:justify-between pt-5">
+          <div className="flex items-center gap-3">
+            <Image
+              src={"/q-chang-logo-short.webp"}
+              height={30}
+              width={30}
+              alt={"Aong Chonprakun - UX Experience"}
+            />
+            <p className="text-gray-300 ">
+              <span className="font-semibold">Q Chang /</span> Front End
+              Developer
+            </p>
+          </div>
+          <p className="text-gray-300 md:pt-0 pt-3">
+            May 2022 - Present, Thailand
+          </p>
+        </div>
+
+        <p className="text-gray-300 pt-5">
+          As a Front-End Developer at Q-Chang, I specialize in Nuxt.js,
+          TypeScript, and Vuetify to create intuitive, responsive web platforms.
+          I’ve implemented key features, optimized performance, and developed a
+          Line Bot with Firebase to enhance customer interactions, delivering
+          seamless user experiences.
+        </p>
+        <div className="flex-col flex sm:flex-row ">
+          <div className="bg-transparent  mt-5 mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
+            Next.js
+          </div>
+          <div className="bg-transparent  mt-5  mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
+            Vue
+          </div>
+          <div className="bg-transparent  mt-5  mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
+            JavaScript
+          </div>
+          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
+            Typescript
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
