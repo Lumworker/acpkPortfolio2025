3645c7b89b274bca3fc69cc48751f35052f47654
@chonprakun <Lumworker@hotmail.com>
2025-01-19 17:50:11 +0700

    Adjust font size of Resume heading for improved visual hierarchy

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -11,7 +11,7 @@ const Resume: React.FC<{}> = () => {
 
   return (
     <section id="Resume" className="text-center">
-      <h2 className="text-white font-semibold text-6xl">📝 Resume !!</h2>
+      <h2 className="text-white font-semibold text-5xl">📝 Resume !!</h2>
       <p className="tracking-[0.5em] text-transparent font-light bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-1xl mt-5">
         GET NOW
       </p>
